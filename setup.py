e04c12cb3403dfabb9db8d655e773b6cde416179
Andryerics <137793417+Andryerics@users.noreply.github.com>
2024-01-16 13:02:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nom_du_module',
+    name='malavida_scraper_python',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
